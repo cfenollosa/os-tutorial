f1dab708b1f429ab87787fb149b8b3048c82a2af
Carlos Fenollosa <carlos.fenollosa@gmail.com>
2014-10-05 12:06:14 +0200

    lesson 4, manipulating the stack

--- 04-bootsector-stack/boot_sect_stack.asm
+++ 04-bootsector-stack/boot_sect_stack.asm
@@ -1,7 +1,7 @@
-mov ah, 0x0e
+mov ah, 0x0e ; tty mode
 
 mov bp, 0x8000 ; this is an address far away from 0x7c00 so that we don't get overwritten
-mov sp, bp
+mov sp, bp ; if the stack is empty then sp points to bp
 
 push 'A'
 push 'B'
