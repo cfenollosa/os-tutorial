18c8989d333cc5c3b7f0d67e582e84335780faa3
Carlos Fenollosa <carlos.fenollosa@gmail.com>
2014-10-16 11:35:20 +0200

    lesson 13 comments

--- 13-kernel-barebones/kernel.c
+++ 13-kernel-barebones/kernel.c
@@ -1,4 +1,4 @@
-/* This will force us to create a kernel entry function */
+/* This will force us to create a kernel entry function instead of jumping to kernel.c:0x00 */
 void dummy_test_entrypoint() {
 }
 
