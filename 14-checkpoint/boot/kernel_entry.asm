418bb11305c7e3dd04358d78ba67ffa08236f1eb
Carlos Fenollosa <cfenollosa@mba.local>
2015-02-03 19:12:51 +0100

    Lesson 18

--- 14-checkpoint/boot/kernel_entry.asm
+++ 14-checkpoint/boot/kernel_entry.asm
@@ -1,1 +1,4 @@
-../../13-kernel-barebones/kernel_entry.asm
+[bits 32]
+[extern main] ; Define calling point. Must have same name as kernel.c 'main' function
+call main ; Calls the C function. The linker will know where it is placed in memory
+jmp $
--- 17-video-scroll/drivers/ports.h
+++ 17-video-scroll/drivers/ports.h
@@ -1,1 +1,4 @@
-../../16-video-driver/drivers/ports.h
+unsigned char port_byte_in (unsigned short port);
+void port_byte_out (unsigned short port, unsigned char data);
+unsigned short port_word_in (unsigned short port);
+void port_word_out (unsigned short port, unsigned short data);
