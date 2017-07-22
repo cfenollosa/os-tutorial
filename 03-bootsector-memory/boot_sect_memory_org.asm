c52e72f647a09245cc2da61783f5c543cab104a5
alhaad <alhaadgokhale@gmail.com>
2017-07-21 20:23:57 -0700

    Fix typo.

--- 03-bootsector-memory/boot_sect_memory_org.asm
+++ 03-bootsector-memory/boot_sect_memory_org.asm
@@ -39,7 +39,7 @@ jmp $ ; infinite loop
 
 the_secret:
     ; ASCII code 0x58 ('X') is stored just before the zero-padding.
-    ; On this code that is at byte 0x2d (check it out using 'xdd file.bin')
+    ; On this code that is at byte 0x2d (check it out using 'xxd file.bin')
     db "X"
 
 ; zero padding and magic bios number
