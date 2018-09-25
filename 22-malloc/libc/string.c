c95d3c158e85f8d73b4629edd176015c27791fa5
RoguePotato <anthony.mercer@hotmail.co.uk>
2018-09-25 19:32:56 +0100

    Fixed hex_to_string to display 'a' character properly.

--- 22-malloc/libc/string.c
+++ 22-malloc/libc/string.c
@@ -29,7 +29,7 @@ void hex_to_ascii(int n, char str[]) {
         tmp = (n >> i) & 0xF;
         if (tmp == 0 && zeros == 0) continue;
         zeros = 1;
-        if (tmp > 0xA) append(str, tmp - 0xA + 'a');
+        if (tmp >= 0xA) append(str, tmp - 0xA + 'a');
         else append(str, tmp + '0');
     }
 
