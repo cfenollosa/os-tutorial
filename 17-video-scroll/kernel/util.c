583671de7b3d8a8dfb1257e5ce068fcd8c2e22f9
David <admin@dxscx.com>
2019-03-21 14:40:55 +0800

    feat: Modified something for Ubuntu run, such as gcc,ld key words and flags

--- 17-video-scroll/kernel/util.c
+++ 17-video-scroll/kernel/util.c
@@ -9,15 +9,22 @@ void memory_copy(char *source, char *dest, int nbytes) {
  * K&R implementation
  */
 void int_to_ascii(int n, char str[]) {
-    int i, sign;
+    int i, sign,length;
     if ((sign = n) < 0) n = -n;
     i = 0;
+    length=0;
     do {
         str[i++] = n % 10 + '0';
+        length++;
     } while ((n /= 10) > 0);
 
     if (sign < 0) str[i++] = '-';
+    //reverse
+    for(int j=0;j<length/2;j++){
+        char t=str[j];
+        str[j]=str[length-j-1];
+        str[length-j-1]=t;
+    }
     str[i] = '\0';
-
-    /* TODO: implement "reverse" */
+    length++;
 }
