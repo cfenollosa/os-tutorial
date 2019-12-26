ac0ff26fd0af798a528d1d9da971453120c1e7a4
CO18326 <42867901+CO18326@users.noreply.github.com>
2019-12-26 17:21:40 +0530

    Update boot_sect_simple.asm
    Thanks for course

--- 01-bootsector-barebones/boot_sect_simple.asm
+++ 01-bootsector-barebones/boot_sect_simple.asm
@@ -1,6 +1,21 @@
 ; A simple boot sector program that loops forever
-loop:
-    jmp loop
 
+mov ah,oeh
+mov si,msg
+
+printing
+mov al,[si]
+add si,1
+or al,0
+je exit
+jmp printing
+exit:
+
+
+
+
+
+
+msg  db "hello world",0
 times 510-($-$$) db 0
 dw 0xaa55
