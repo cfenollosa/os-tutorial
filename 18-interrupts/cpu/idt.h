8b16fc9effdcc48425d39815b4279e4e0de75f71
Akshaylal S <40035782+Akshaylal@users.noreply.github.com>
2021-05-24 20:44:05 +0530

    Moved idt and idt reg from idt.h to idt.c
    Moved idt and idt reg from idt.h to idt.c to fix the make error: multiple definitions of ...

--- 18-interrupts/cpu/idt.h
+++ 18-interrupts/cpu/idt.h
@@ -28,8 +28,6 @@ typedef struct {
 } __attribute__((packed)) idt_register_t;
 
 #define IDT_ENTRIES 256
-idt_gate_t idt[IDT_ENTRIES];
-idt_register_t idt_reg;
 
 
 /* Functions implemented in idt.c */
